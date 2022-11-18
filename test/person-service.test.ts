9415e61367d64cc7523b8bd2bbb54d82b560c51a
Raymond Yong <raymond.yong@nl.abnamro.com>
2022-11-18 10:36:30 +0100

    fix linting

--- test/person-service.test.ts
+++ test/person-service.test.ts
@@ -3,15 +3,15 @@ import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as PersonService from '../lib/person-service-stack';
 
 test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new PersonService.PersonServiceStack(app, 'MyTestStack');
-  // THEN
+    const app = new cdk.App();
+    // WHEN
+    const stack = new PersonService.PersonServiceStack(app, 'MyTestStack');
+    // THEN
 
-  const template = Template.fromStack(stack);
+    const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+    template.hasResourceProperties('AWS::SQS::Queue', {
+        VisibilityTimeout: 300,
+    });
+    template.resourceCountIs('AWS::SNS::Topic', 1);
 });
