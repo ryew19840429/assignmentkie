0d8518b412b8f0be5d56dc160737edcbe610a2b6
Raymond Yong <raymond.yong@nl.abnamro.com>
2022-11-21 16:36:59 +0100

    add tests

---
+++ test/src/domain/person/services/person.services.test.ts
@@ -0,0 +1,41 @@
+import { Person } from '../../../../../src/domain/person/models/person.model';
+import { PersonService } from '../../../../../src/domain/person/services/person.services';
+
+import { PutItemInput } from 'aws-sdk/clients/dynamodb';
+import AWS = require('aws-sdk');
+import AWSMock = require('aws-sdk-mock');
+
+beforeEach(() => {
+    process.env.AWS_REGION = 'eu-west-1';
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('PersonService', () => {
+    test('method: handlerResponse', () => {
+        const handlerResponseResult = PersonService.handlerResponse('test response', 200);
+        expect(handlerResponseResult).toStrictEqual({ body: 'test response', statusCode: 200 });
+    });
+
+    test('method: putPerson', async () => {
+        AWSMock.setSDKInstance(AWS);
+        AWSMock.mock('DynamoDB.DocumentClient', 'put', (params: PutItemInput, callback: any) => {
+            callback(null, {});
+        });
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+        const personMock: Person = {
+            phoneNumber: 123456,
+            firstName: 'john',
+            lastName: 'doe',
+            address: 'somewhere',
+        };
+        const putSpy = jest.spyOn(dynamodb, 'put');
+        await PersonService.putPerson(personMock, 'testTableName', dynamodb);
+        expect(putSpy).toBeCalledWith({
+            Item: { address: 'somewhere', firstName: 'john', lastName: 'doe', phoneNumber: 123456 },
+            TableName: 'testTableName',
+        });
+    });
+});
